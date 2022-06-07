f00e7f2aaad428b9567c0e45d9b5e3b51a2efbb1
Paul Nolan <92262660+nalonluap@users.noreply.github.com>
2022-06-07 12:50:22 +0300

    client-server

--- server/routes/api.js
+++ server/routes/api.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 router.get('/', async (req, res) => {
 
-    res.sendFile(path.join(__dirname, '..', '..', 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, '..', '..', 'client', 'dist', 'index.html'));
 });
 
 
